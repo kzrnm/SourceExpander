309c0e5828f18b3e19dc897b3fb43651cac7ecd9
naminodarie <gengesa@gmail.com>
2020-11-23 03:38:44 +0900

    add ExpandGenerator Test

--- Test/SourceExpander.Generator.Test/ExpandGeneratorTest.cs
+++ Test/SourceExpander.Generator.Test/ExpandGeneratorTest.cs
@@ -69,25 +69,32 @@ class Program2
             driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
             diagnostics.Should().BeEmpty();
             outputCompilation.SyntaxTrees.Should().HaveCount(syntaxTrees.Length + 1);
-            outputCompilation.SyntaxTrees
-                .Single(tree => tree.FilePath.EndsWith("SourceExpander.Expanded.cs"))
-                .ToString()
+            var d = outputCompilation.SyntaxTrees
                 .Should()
-                .NotContain("System.Reflection")
-                .And
-                .NotContain("System.Math")
-                .And
-                .Contain("class Xorshift")
-                .And
-                .Contain("class Put2");
+                .ContainSingle(tree => tree.FilePath.EndsWith("SourceExpander.Expanded.cs"))
+                .Which
+                .ToString()
+                .Replace("\r\n", "\n")
+                .Replace("\\r\\n", "\\n")
+                .Should()
+                .Be("using System.Collections.Generic;\n" +
+                    "namespace Expanded{\n" +
+                        "public class SourceCode{ public string Path; public string Code; }\n" +
+                        "public static class Expanded{\n" +
+                        "public static IReadOnlyDictionary<string, SourceCode> Files { get; } = new Dictionary<string, SourceCode>{\n" +
+                        "{\"/home/source/Program.cs\", new SourceCode{ Path=\"/home/source/Program.cs\", " +
+                            "Code=\"using SampleLibrary;\\nusing System;\\nusing System.Diagnostics;\\nclass Program\\n{\\n    static void Main()\\n    {\\n        Console.WriteLine(42);\\n        Put.WriteRandom();\\n#if !EXPAND_GENERATOR\\n        Console.WriteLine(24);\\n#end if\\n    }\\n}\\n" +
+                                "#region Expanded\\nnamespace SampleLibrary { public static class Put { private static readonly Xorshift rnd = new Xorshift(); public static void WriteRandom() => Trace.WriteLine(rnd.Next()); } } \\nnamespace SampleLibrary { public class Xorshift : Random { private uint x = 123456789; private uint y = 362436069; private uint z = 521288629; private uint w; private static readonly Random rnd = new Random(); public Xorshift() : this(rnd.Next()) { } public Xorshift(int seed) { w = (uint)seed; } protected override double Sample() => InternalSample() * (1.0 / uint.MaxValue); private uint InternalSample() { uint t = x ^ (x << 11); x = y; y = z; z = w; return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); } } } \\n#endregion Expanded\" } },\n" +
+                        "{\"/home/source/Program2.cs\", new SourceCode{ Path=\"/home/source/Program2.cs\", " +
+                            "Code=\"using SampleLibrary;\\nusing System;\\nusing System.Diagnostics;\\nclass Program2\\n{\\n    static void Main()\\n    {\\n        Console.WriteLine(42);\\n        Put2.Write();\\n    }\\n}\\n" +
+                                "#region Expanded\\nnamespace SampleLibrary { public static class Put2 { public static void Write() => Put.WriteRandom(); } } \\nnamespace SampleLibrary { public static class Put { private static readonly Xorshift rnd = new Xorshift(); public static void WriteRandom() => Trace.WriteLine(rnd.Next()); } } \\nnamespace SampleLibrary { public class Xorshift : Random { private uint x = 123456789; private uint y = 362436069; private uint z = 521288629; private uint w; private static readonly Random rnd = new Random(); public Xorshift() : this(rnd.Next()) { } public Xorshift(int seed) { w = (uint)seed; } protected override double Sample() => InternalSample() * (1.0 / uint.MaxValue); private uint InternalSample() { uint t = x ^ (x << 11); x = y; y = z; z = w; return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); } } } \\n#endregion Expanded\" } },\n" +
+                    "};\n" +
+                "}}\n");
         }
         [Fact]
         public void GenerateNotFoundTest()
         {
-            var syntaxTrees = new[]
-            {
-                CSharpSyntaxTree.ParseText(
-                    @"using System;
+            const string code = @"using System;
 using System.Reflection;
 using static System.Math;
 
@@ -97,7 +104,11 @@ class Program
     {
         Console.WriteLine(42);
     }
-}",
+}";
+            var syntaxTrees = new[]
+            {
+                CSharpSyntaxTree.ParseText(
+                    code,
                     options: new CSharpParseOptions(documentationMode:DocumentationMode.None),
                     path: "/home/source/Program.cs"),
             };
@@ -120,17 +131,24 @@ class Program
                 .Id
                 .Should().Be("EXPAND0001");
             outputCompilation.SyntaxTrees.Should().HaveCount(syntaxTrees.Length + 1);
+
             outputCompilation.SyntaxTrees
-                .Single(tree => tree.FilePath.EndsWith("SourceExpander.Expanded.cs"))
-                .ToString()
-                .Should()
-                .NotContain("System.Reflection")
-                .And
-                .NotContain("System.Math")
-                .And
-                .NotContain("class Xorshift")
-                .And
-                .NotContain("class Put2");
+            .Should()
+            .ContainSingle(tree => tree.FilePath.EndsWith("SourceExpander.Expanded.cs"))
+            .Which
+            .ToString()
+            .Replace("\r\n", "\n")
+            .Replace("\\r\\n", "\\n")
+            .Should()
+            .Be("using System.Collections.Generic;\n" +
+                "namespace Expanded{\n" +
+                    "public class SourceCode{ public string Path; public string Code; }\n" +
+                    "public static class Expanded{\n" +
+                    "public static IReadOnlyDictionary<string, SourceCode> Files { get; } = new Dictionary<string, SourceCode>{\n" +
+                    "{\"/home/source/Program.cs\", new SourceCode{ Path=\"/home/source/Program.cs\", Code=\"using System;\\n" +
+                    "class Program\\n{\\n    static void Main()\\n    {\\n        Console.WriteLine(42);\\n    }\\n}\\n#region Expanded\\n#endregion Expanded\" } },\n" +
+                "};\n" +
+            "}}\n");
         }
 
         static IEnumerable<string> GetSampleDllPaths()
