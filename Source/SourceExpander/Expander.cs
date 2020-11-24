6b6343bc6249b4e48b1a52c58482501cb063c54c
naminodarie <gengesa@gmail.com>
2020-11-24 12:37:17 +0900

    clear expander

---
+++ Source/SourceExpander/Expander.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace SourceExpander
+{
+    public static class Expander
+    {
+        public static void Expand([CallerFilePath] string inputFilePath = null, string outputFilePath = null)
+        {
+
+        }
+    }
+    public enum ExpandMethod
+    {
+        /// <summary>
+        /// Write all embeded type(fast)
+        /// </summary>
+        All,
+        /// <summary>
+        /// Found type name by SyntaxTree(slow)
+        /// </summary>
+        NameSyntax,
+        /// <summary>
+        /// Found type name by Compilation(too slow)
+        /// </summary>
+        Strict,
+    }
+}
