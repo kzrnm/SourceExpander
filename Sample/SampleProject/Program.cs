3dde6e16cc8ee955d6c4abc5972b32f6fc134e20
naminodarie <gengesa@gmail.com>
2020-11-27 20:39:17 +0900

    update sample

--- Sample/SampleProject/Program.cs
+++ Sample/SampleProject/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine(Expanded.Expanded.Files);
+        Console.WriteLine(SourceExpander.Expanded.ExpandedContainer.Files);
         Put2.Write();
     }
 }
