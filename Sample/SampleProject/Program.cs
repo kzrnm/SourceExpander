780728bfaf97b292966fbdef95fd14d693a35e60
naminodarie <gengesa@gmail.com>
2020-11-29 01:46:25 +0900

    [WIP]Sample

--- Sample/SampleProject/Program.cs
+++ Sample/SampleProject/Program.cs
@@ -6,6 +6,7 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine(typeof(SourceExpander.Expanded.SourceCode));
         Console.WriteLine(SourceExpander.Expanded.ExpandedContainer.Files);
         Put2.Write();
     }
