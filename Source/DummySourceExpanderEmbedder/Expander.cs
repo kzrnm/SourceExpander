e461a1e021f61cbc8c673e23aeac68864423bca3
naminodarie <gengesa@gmail.com>
2020-11-30 01:50:20 +0900

    embedding dummy SourceExpander code

---
+++ Source/DummySourceExpanderEmbedder/Expander.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace SourceExpander
+{
+    public static class Expander
+    {
+        [Conditional("EXPANDER")]
+        public static void Expand(string inputFilePath = null, string outputFilePath = null) { }
+        public static string ExpandString(string inputFilePath = null) { return ""; }
+    }
+}
