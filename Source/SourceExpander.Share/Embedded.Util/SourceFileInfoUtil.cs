90b763eaa3c80fa4fc943af9d850683f0fb57c0a
naminodarie <gengesa@gmail.com>
2020-12-01 14:02:24 +0900

    remove unused using

--- Source/SourceExpander.Share/Embedded.Util/SourceFileInfoUtil.cs
+++ Source/SourceExpander.Share/Embedded.Util/SourceFileInfoUtil.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
 using Kzrnm.Convert.Base32768;
-using Microsoft.CodeAnalysis.CSharp;
 #nullable enable
 namespace SourceExpander
 {
