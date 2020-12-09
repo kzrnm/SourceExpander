1f53078830c596d8272b988ccadf71055b9bfea3
naminodarie <gengesa@gmail.com>
2020-12-10 00:02:59 +0900

    Enumerate TypeName

---
+++ Test/SourceExpander.Share.Test/TestUtil.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace SourceExpander.Share.Test
+{
+    static class TestUtil
+    {
+        public static readonly MetadataReference[] defaultMetadatas = GetDefaulMetadatas().ToArray();
+
+        private static IEnumerable<MetadataReference> GetDefaulMetadatas()
+        {
+            var directory = Path.GetDirectoryName(typeof(object).Assembly.Location);
+            foreach (var file in Directory.EnumerateFiles(directory, "System*.dll"))
+            {
+                yield return MetadataReference.CreateFromFile(file);
+            }
+        }
+
+    }
+}
