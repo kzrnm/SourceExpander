607897349f7a5572d51b879e2f0f9c0d6bb41374
naminodarie <gengesa@gmail.com>
2020-12-12 01:21:17 +0900

    Compress DummyExpander

--- Source/SourceExpander/AssemblyInfo.cs
+++ Source/SourceExpander/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyMetadata("SourceExpander.EmbeddedSourceCode.GZipBase32768", "㘅桠ҠҠԀስ䛺鈣㹆敪躩餸乔ጸ䆴䲻㱲鲸㡜牴邃䐀耧㗱仝幉鋥䄆㻡ꐃ谳镄籇咘頳釡臤宻邉䊪ᘥ㧯笜匒甼汼跐骔莢鰊ቼ⺷萋㖯禱羊耴劾▍痗痪燮诒颢傈釳ꅻ茥誇䤔ꆣ岎褓臚㩜䚳詢嵹滁ᔜ鞟ꃄ憐ᕥ瘕礵╢換蜱㨭㣎祇ꄃ䓴昖飊鶭鹅ꗢ澛惣㑴幂䁏妬䬈鞘應癵愌珦矖㷰礉疝嘞❖彿様㒮獬ጾᅕꆀ劶濎䛻丁䁫㩃檊剜簌滖⎘埌纼玗ꛓ喑㒀ᖀƁ")]
+[assembly: AssemblyMetadata("SourceExpander.EmbeddedSourceCode.GZipBase32768", "㘅桠ҠҠҠ偕䟶鄣㹆斥嫔韄㠽Ꮦ⥝潾䯬珦ᘏ㵁䓈諓㨰塶柟薳礡茍歑垏璥䎪桩葞㹛癥罦賬跄殱徸⥅橈讈我秦笊冺㴎欯ꉝ劭䞍擂漝箰蒏䧹鷯⢦㔩噳疪瘯䐕興尓燘秒⡄㨼矉蒄晍答␛壁詁斦刓⥵㟛䑩ᖶ✎镖鑣緃蒛糭㸗猏痼旯旄㲽繲䃪⫢鲘䚣漲垽䲖眡臕勦⋙郻♆雙魝幼形敇濫瀼┠ꐭ珗߄ꜘ肪䥲棄笀㘏堿颅俿凷劾鍤䵴疋揸儰暠ҿ")]
 [assembly: AssemblyMetadata("SourceExpander.EmbeddedLanguageVersion", "4")]
