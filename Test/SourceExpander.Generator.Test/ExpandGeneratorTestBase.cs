020fc100f83b08e777ab7ad6e12f0ebda2d16eee
naminodarie <gengesa@gmail.com>
2021-03-16 01:52:12 +0900

    Delete Test/Utils/GeneratorTestBase.cs

--- Test/SourceExpander.Generator.Test/ExpandGeneratorTestBase.cs
+++ Test/SourceExpander.Generator.Test/ExpandGeneratorTestBase.cs
@@ -1,16 +1,150 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
 using SourceExpander.Expanded;
 
 namespace SourceExpander.Generator
 {
-    public class ExpandGeneratorTestBase : GeneratorTestBase
+    public class ExpandGeneratorTestBase
     {
+        public class Test : CSharpSourceGeneratorTest<ExpandGenerator>
+        {
+            public Test()
+            {
+                ParseOptions = ParseOptions.WithLanguageVersion(EmbeddedLanguageVersionEnum);
+                ReferenceAssemblies = ReferenceAssemblies.Net.Net50.AddPackages(Packages);
+            }
+        }
+
+        internal static Solution CreateOtherReference(Solution solution,
+            ProjectId projectId,
+            SourceFileCollection documents,
+            CSharpCompilationOptions compilationOptions = null)
+        {
+            if (compilationOptions is null)
+                compilationOptions = new(OutputKind.DynamicallyLinkedLibrary);
+
+            var targetProject = solution.GetProject(projectId);
+
+            var project = solution.AddProject("Other", "Other", "C#")
+                .WithMetadataReferences(targetProject.MetadataReferences)
+                .WithCompilationOptions(compilationOptions);
+            foreach (var (filename, content) in documents)
+            {
+                project = project.AddDocument(Path.GetFileNameWithoutExtension(filename), content, filePath: filename).Project;
+            }
+
+            return project.Solution.AddProjectReference(projectId, new(project.Id));
+        }
+
+
+        internal static ImmutableArray<PackageIdentity> Packages
+            = ImmutableArray.Create(new PackageIdentity("SourceExpander.Core", "2.6.0"));
+        public static string EmbedderVersion => typeof(ExpandGenerator).Assembly.GetName().Version.ToString();
+        private static readonly LanguageVersion EmbeddedLanguageVersionEnum = LanguageVersion.CSharp9;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static CSharpCompilation CreateCompilation(
+            IEnumerable<SyntaxTree> syntaxTrees,
+            CSharpCompilationOptions compilationOptions,
+            IEnumerable<MetadataReference> additionalMetadatas = null,
+            string assemblyName = "TestAssembly")
+        {
+            additionalMetadatas ??= Array.Empty<MetadataReference>();
+            return CSharpCompilation.Create(
+                assemblyName: assemblyName,
+                syntaxTrees: syntaxTrees,
+                references: DefaultMetadatas.Concat(additionalMetadatas),
+                options: compilationOptions);
+        }
+
+        private static IEnumerable<MetadataReference> DefaultMetadatas { get; } = GetDefaultMetadatas();
+        private static IEnumerable<MetadataReference> GetDefaultMetadatas()
+        {
+            var directory = Path.GetDirectoryName(typeof(object).Assembly.Location);
+            foreach (var file in Directory.EnumerateFiles(directory, "System*.dll"))
+            {
+                yield return MetadataReference.CreateFromFile(file);
+            }
+        }
+
+        protected static GeneratorResult RunGenerator(
+               Compilation compilation,
+               ISourceGenerator generator,
+               IEnumerable<AdditionalText> additionalTexts = null,
+               CSharpParseOptions parseOptions = null,
+               AnalyzerConfigOptionsProvider optionsProvider = null)
+            => RunGenerator(compilation, new[] { generator }, additionalTexts, parseOptions, optionsProvider);
+        protected static GeneratorResult RunGenerator(
+               Compilation compilation,
+               IEnumerable<ISourceGenerator> generators,
+               IEnumerable<AdditionalText> additionalTexts = null,
+               CSharpParseOptions parseOptions = null,
+               AnalyzerConfigOptionsProvider optionsProvider = null)
+        {
+            var driver = CSharpGeneratorDriver.Create(generators, additionalTexts, parseOptions, optionsProvider);
+            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+
+            return new GeneratorResult((CSharpCompilation)outputCompilation, diagnostics,
+                outputCompilation.SyntaxTrees.Except(compilation.SyntaxTrees, new SyntaxComparer())
+                .Cast<CSharpSyntaxTree>().ToImmutableArray());
+        }
+
+        private class SyntaxComparer : IEqualityComparer<SyntaxTree>
+        {
+            public bool Equals(SyntaxTree x, SyntaxTree y) => x.IsEquivalentTo(y);
+            public int GetHashCode(SyntaxTree obj) => obj.FilePath?.GetHashCode() ?? 0;
+        }
+        protected class GeneratorResult
+        {
+            public GeneratorResult(
+             CSharpCompilation outputCompilation,
+             ImmutableArray<Diagnostic> diagnostics,
+             ImmutableArray<CSharpSyntaxTree> addedSyntaxTrees)
+            {
+                OutputCompilation = outputCompilation;
+                Diagnostics = diagnostics;
+                AddedSyntaxTrees = addedSyntaxTrees;
+            }
+            public CSharpCompilation OutputCompilation { get; }
+            public ImmutableArray<Diagnostic> Diagnostics { get; }
+            public ImmutableArray<CSharpSyntaxTree> AddedSyntaxTrees { get; }
+        }
         public static IReadOnlyDictionary<string, SourceCode> GetExpandedFilesWithCore(Compilation compilation)
             => (IReadOnlyDictionary<string, SourceCode>)GetExpandedFiles(compilation);
         internal static object GetExpandedFiles(Compilation compilation)
