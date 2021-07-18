7ea08f3eb30c3fe32814dae9d81f108275da402b
naminodarie <gengesa@gmail.com>
2021-07-18 18:21:16 +0900

    Move util

---
+++ Test/Utils/EnvironmentUtil.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace SourceExpander
+{
+    public static class EnvironmentUtil
+    {
+        public static string JoinByStringBuilder(params string[] strs)
+        {
+            var sb = new StringBuilder();
+            foreach (var s in strs)
+                sb.AppendLine(s);
+            return sb.ToString();
+        }
+        public static string ReplaceEOL(this string str) => str.Replace('\n', '\r');
+    }
+}
