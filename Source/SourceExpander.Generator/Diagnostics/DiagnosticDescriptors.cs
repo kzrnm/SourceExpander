98edcdc5dff379eceaa3c2470b2f213c09cf1f8d
kzrnm <gengesa@gmail.com>
2023-09-12 00:10:41 +0900

    Fix RS2003
    https://github.com/dotnet/roslyn-analyzers/issues/5828

--- Source/SourceExpander.Generator/Diagnostics/DiagnosticDescriptors.cs
+++ Source/SourceExpander.Generator/Diagnostics/DiagnosticDescriptors.cs
@@ -15,7 +15,7 @@ namespace SourceExpander
 
         public static Diagnostic EXPAND0001_UnknownError(string message)
              => Diagnostic.Create(EXPAND0001_UnknownError_Descriptor, Location.None, message);
-        private static readonly DiagnosticDescriptor EXPAND0001_UnknownError_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0001_UnknownError_Descriptor = new DiagnosticDescriptor(
             "EXPAND0001",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0001_Title),
@@ -30,7 +30,7 @@ namespace SourceExpander
             true);
         public static Diagnostic EXPAND0002_ExpanderVersion(Version expanderVersion, string assemblyName, Version assemblyEmbedderVersion)
              => Diagnostic.Create(EXPAND0002_ExpanderVersion_Descriptor, Location.None, expanderVersion.ToString(), assemblyName, assemblyEmbedderVersion.ToString());
-        private static readonly DiagnosticDescriptor EXPAND0002_ExpanderVersion_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0002_ExpanderVersion_Descriptor = new DiagnosticDescriptor(
             "EXPAND0002",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0002_Title),
@@ -45,7 +45,7 @@ namespace SourceExpander
             true);
         public static Diagnostic EXPAND0003_NotFoundEmbedded()
              => Diagnostic.Create(EXPAND0003_NotFoundEmbedded_Descriptor, Location.None);
-        private static readonly DiagnosticDescriptor EXPAND0003_NotFoundEmbedded_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0003_NotFoundEmbedded_Descriptor = new DiagnosticDescriptor(
             "EXPAND0003",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0003_Title),
@@ -60,7 +60,7 @@ namespace SourceExpander
             true);
         public static Diagnostic EXPAND0004_MustBeNewerThanCSharp3()
              => Diagnostic.Create(EXPAND0004_MustBeNewerThanCSharp3_Descriptor, Location.None);
-        private static readonly DiagnosticDescriptor EXPAND0004_MustBeNewerThanCSharp3_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0004_MustBeNewerThanCSharp3_Descriptor = new DiagnosticDescriptor(
             "EXPAND0004",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0004_Title),
@@ -75,7 +75,7 @@ namespace SourceExpander
             true);
         public static Diagnostic EXPAND0005_NewerCSharpVersion(LanguageVersion expanderVersion, string assemblyName, LanguageVersion assemblyEmbedderVersion)
              => Diagnostic.Create(EXPAND0005_NewerCSharpVersion_Descriptor, Location.None, expanderVersion.ToDisplayString(), assemblyName, assemblyEmbedderVersion.ToDisplayString());
-        private static readonly DiagnosticDescriptor EXPAND0005_NewerCSharpVersion_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0005_NewerCSharpVersion_Descriptor = new DiagnosticDescriptor(
             "EXPAND0005",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0005_Title),
@@ -90,7 +90,7 @@ namespace SourceExpander
             true);
         public static Diagnostic EXPAND0006_AllowUnsafe(string assemblyName)
              => Diagnostic.Create(EXPAND0006_AllowUnsafe_Descriptor, Location.None, assemblyName);
-        private static readonly DiagnosticDescriptor EXPAND0006_AllowUnsafe_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0006_AllowUnsafe_Descriptor = new DiagnosticDescriptor(
             "EXPAND0006",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0006_Title),
@@ -106,7 +106,7 @@ namespace SourceExpander
         public static Diagnostic EXPAND0007_ParseConfigError(string? configFile, string message)
              => Diagnostic.Create(EXPAND0007_ParseConfigError_Descriptor,
                  AdditionalFileLocation(configFile), configFile ?? "Any of configs", message);
-        private static readonly DiagnosticDescriptor EXPAND0007_ParseConfigError_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0007_ParseConfigError_Descriptor = new DiagnosticDescriptor(
             "EXPAND0007",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0007_Title),
@@ -121,7 +121,7 @@ namespace SourceExpander
             true);
         public static Diagnostic EXPAND0008_EmbeddedDataError(string? assemblyName, string key, string value)
              => Diagnostic.Create(EXPAND0008_EmbeddedDataError_Descriptor, Location.None, assemblyName, key, value);
-        private static readonly DiagnosticDescriptor EXPAND0008_EmbeddedDataError_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0008_EmbeddedDataError_Descriptor = new DiagnosticDescriptor(
             "EXPAND0008",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0008_Title),
@@ -136,7 +136,7 @@ namespace SourceExpander
             true);
         public static Diagnostic EXPAND0009_MetadataEmbeddingFileNotFound(string fileName)
              => Diagnostic.Create(EXPAND0009_MetadataEmbeddingFileNotFound_Descriptor, Location.None, fileName);
-        private static readonly DiagnosticDescriptor EXPAND0009_MetadataEmbeddingFileNotFound_Descriptor = new(
+        private static readonly DiagnosticDescriptor EXPAND0009_MetadataEmbeddingFileNotFound_Descriptor = new DiagnosticDescriptor(
             "EXPAND0009",
             new LocalizableResourceString(
                 nameof(DiagnosticsResources.EXPAND0009_Title),
