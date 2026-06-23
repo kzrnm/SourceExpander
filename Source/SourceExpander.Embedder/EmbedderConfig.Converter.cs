e12c8a311dcaa553f2c8af6dd446b7d84f106b10
kzrnm <gengesa@gmail.com>
2026-06-24 05:29:01 +0900

    Add LanguageVersion to Embedder.Config

--- Source/SourceExpander.Embedder/EmbedderConfig.Converter.cs
+++ Source/SourceExpander.Embedder/EmbedderConfig.Converter.cs
@@ -1,80 +1,91 @@
 ﻿using System;
 using System.Collections.Immutable;
 using System.Runtime.Serialization;
+using Microsoft.CodeAnalysis.CSharp;
 
-namespace SourceExpander
+namespace SourceExpander;
+
+internal partial record EmbedderConfig
 {
-    internal partial record EmbedderConfig
+    [DataContract]
+    private class EmbedderConfigData
     {
-        [DataContract]
-        private class EmbedderConfigData
-        {
-            [DataMember(Name = "enabled")]
-            public bool? Enabled { set; get; }
-            [DataMember(Name = "include")]
-            public string[]? Include { set; get; }
-            [DataMember(Name = "exclude")]
-            public string[]? Exclude { set; get; }
-            [DataMember(Name = "embedding-type")]
-            public string? EmbeddingType { set; get; }
-            [DataMember(Name = "exclude-attributes")]
-            public string[]? ExcludeAttributes { set; get; }
-            [DataMember(Name = "minify-level")]
-            public string? MinifyLevel { set; get; }
-            [DataMember(Name = "remove-conditional")]
-            public string[]? RemoveConditional { set; get; }
-            [DataMember(Name = "embedding-source-class-name")]
-            public string? EmbeddingSourceClassName { set; get; }
-            [DataMember(Name = "embedding-filename-type")]
-            public string? EmbeddingFileNameType { set; get; }
+        [DataMember(Name = "enabled")]
+        public bool? Enabled { set; get; }
+        [DataMember(Name = "include")]
+        public string[]? Include { set; get; }
+        [DataMember(Name = "exclude")]
+        public string[]? Exclude { set; get; }
+        [DataMember(Name = "embedding-type")]
+        public string? EmbeddingType { set; get; }
+        [DataMember(Name = "exclude-attributes")]
+        public string[]? ExcludeAttributes { set; get; }
+        [DataMember(Name = "minify-level")]
+        public string? MinifyLevel { set; get; }
+        [DataMember(Name = "remove-conditional")]
+        public string[]? RemoveConditional { set; get; }
+        [DataMember(Name = "embedding-source-class-name")]
+        public string? EmbeddingSourceClassName { set; get; }
+        [DataMember(Name = "embedding-filename-type")]
+        public string? EmbeddingFileNameType { set; get; }
 
-            [DataMember(Name = "expand-in-library")]
-            public bool? ExpandInLibrary { set; get; }
+        [DataMember(Name = "language-version")]
+        public object? LanguageVersion { set; get; }
 
-            [Obsolete]
-            [DataMember(Name = "expanding-symbol")]
-            public string? ExpandingSymbol { set; get; }
-            [Obsolete]
-            [DataMember(Name = "embedding-source-class")]
-            public object? EmbeddingSourceClass { set; get; }
-            [Obsolete]
-            [DataMember(Name = "enable-minify")]
-            public bool? EnableMinify { set; get; }
+        [DataMember(Name = "expand-in-library")]
+        public bool? ExpandInLibrary { set; get; }
 
-            private EmbeddingType ParsedEmbeddingType
-                => Enum.TryParse(EmbeddingType, true, out EmbeddingType r) ? r : SourceExpander.EmbeddingType.GZipBase32768;
-            private MinifyLevel ParsedMinifyLevel
-                => Enum.TryParse(MinifyLevel, true, out MinifyLevel r) ? r : SourceExpander.MinifyLevel.Default;
-            private EmbeddingFileNameType ParsedEmbeddingFileNameType
-                => Enum.TryParse(EmbeddingFileNameType, true, out EmbeddingFileNameType r) ? r : SourceExpander.EmbeddingFileNameType.WithoutCommonPrefix;
+        [Obsolete]
+        [DataMember(Name = "expanding-symbol")]
+        public string? ExpandingSymbol { set; get; }
+        [Obsolete]
+        [DataMember(Name = "embedding-source-class")]
+        public object? EmbeddingSourceClass { set; get; }
+        [Obsolete]
+        [DataMember(Name = "enable-minify")]
+        public bool? EnableMinify { set; get; }
 
-            public EmbedderConfig ToImmutable() =>
-                new(
-                        Enabled ?? true,
-                        Include,
-                        Exclude,
-                        ParsedEmbeddingType,
-                        ExcludeAttributes,
-                        ParsedMinifyLevel,
-                        RemoveConditional,
-                        EmbeddingSourceClassName,
-                        ParsedEmbeddingFileNameType,
-                        expandInLibrary: ExpandInLibrary,
-                        obsoleteConfigProperties: GetObsoleteConfigProperties());
+        private EmbeddingType ParsedEmbeddingType
+            => Enum.TryParse(EmbeddingType, true, out EmbeddingType r) ? r : SourceExpander.EmbeddingType.GZipBase32768;
+        private MinifyLevel ParsedMinifyLevel
+            => Enum.TryParse(MinifyLevel, true, out MinifyLevel r) ? r : SourceExpander.MinifyLevel.Default;
+        private EmbeddingFileNameType ParsedEmbeddingFileNameType
+            => Enum.TryParse(EmbeddingFileNameType, true, out EmbeddingFileNameType r) ? r : SourceExpander.EmbeddingFileNameType.WithoutCommonPrefix;
 
-            private ImmutableArray<ObsoleteConfigProperty> GetObsoleteConfigProperties()
+        private LanguageVersion? ParsedLanguageVersion
+            => LanguageVersion?.ToString() switch
             {
-                var builder = ImmutableArray.CreateBuilder<ObsoleteConfigProperty>();
+                { } version when LanguageVersionFacts.TryParse(version, out var result) => result.MapSpecifiedToEffectiveVersion(),
+                _ => null,
+            };
+
+        public EmbedderConfig ToImmutable() =>
+            new(
+                    Enabled ?? true,
+                    Include,
+                    Exclude,
+                    ParsedEmbeddingType,
+                    ExcludeAttributes,
+                    ParsedMinifyLevel,
+                    RemoveConditional,
+                    EmbeddingSourceClassName,
+                    ParsedEmbeddingFileNameType,
+                    languageVersion: ParsedLanguageVersion,
+                    expandInLibrary: ExpandInLibrary,
+                    obsoleteConfigProperties: GetObsoleteConfigProperties());
+
+        private ImmutableArray<ObsoleteConfigProperty> GetObsoleteConfigProperties()
+        {
+            var builder = ImmutableArray.CreateBuilder<ObsoleteConfigProperty>();
 #pragma warning disable CS0612
-                if (EnableMinify.HasValue)
-                    builder.Add(ObsoleteConfigProperty.EnableMinify);
-                if (EmbeddingSourceClass != null)
-                    builder.Add(ObsoleteConfigProperty.EmbeddingSourceClass);
-                if (ExpandingSymbol != null)
-                    builder.Add(ObsoleteConfigProperty.ExpandingSymbol);
+            if (EnableMinify.HasValue)
+                builder.Add(ObsoleteConfigProperty.EnableMinify);
+            if (EmbeddingSourceClass != null)
+                builder.Add(ObsoleteConfigProperty.EmbeddingSourceClass);
+            if (ExpandingSymbol != null)
+                builder.Add(ObsoleteConfigProperty.ExpandingSymbol);
 #pragma warning restore CS0612
-                return builder.ToImmutable();
-            }
+            return builder.ToImmutable();
         }
     }
 }
