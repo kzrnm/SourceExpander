b38bc23037ef21a650862eeda70e6358521bb124
kzrnm <gengesa@gmail.com>
2026-06-30 22:49:43 +0900

    Fix schema path

--- Source/SourceExpander.Console/Commands/SourceExpanderCommand.Embedded.cs
+++ Source/SourceExpander.Console/Commands/SourceExpanderCommand.Embedded.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection.Metadata;
-using System.Reflection.PortableExecutable;
 using System.Threading.Tasks;
 
 namespace SourceExpander;
