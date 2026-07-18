041ed53a82f193e5a24bd52c0e85475bcc95f673
kzrnm <gengesa@gmail.com>
2026-07-18 14:25:24 +0900

    Remove PackJson

---
+++ Source/Sandbox/Sample.Packing/P.cs
@@ -0,0 +1,23 @@
+﻿
+class P
+{
+    public static int Num =>
+#if NET10_0
+        10
+#elif NET9_0
+        9
+#elif NET8_0
+        8
+#elif NET7_0
+        7
+#elif NET6_0
+        6
+#elif NET5_0
+        5
+#elif NETSTANDARD2_1
+        1
+#elif NETSTANDARD2_0
+        0
+#endif
+        ;
+}
